78ec75451b33753980ea0f8be4092b0d56aa5977
yoyoyiyir <91074282+yoyoyiyir@users.noreply.github.com>
2021-10-15 23:33:52 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const battles = require('./auto-gather');
+const chalk = require('chalk');
+
+const sleepingTimeInMinutes = process.env.MINUTES_GATHER_INTERVAL || 30;
+const sleepingTime = sleepingTimeInMinutes * 60000;
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+(async () => {
+	try {
+		const accountusers = process.env.ACCOUNT.split(',');
+		const accounts = accountusers;
+		console.log(' Loaded ' + chalk.yellow(accounts.length) + ' Accounts')
+		console.log(' Accounts: ' + chalk.greenBright(accounts))
+    
+    while (true) {
+    	for (let i = 0; i < accounts.length; i++) {
+			process.env['ACCOUNT'] = accountusers[i];
+			console.log('Gathering battles of: '+chalk.green(accountusers[i]))
+			await battles.battlesList(accountusers[i]).then(x=>x).catch(() => console.log('Unable to gather data for local.'));  
+		}
+	console.log('Waiting for the next gather in', sleepingTime / 1000 / 60, ' minutes at ', new Date(Date.now() + sleepingTime).toLocaleString());
+	await sleep(sleepingTime);
+    }
+	} catch (e) {
+    console.log(' Routine error at: ', new Date().toLocaleString(), e)
+  }
+
+})()
